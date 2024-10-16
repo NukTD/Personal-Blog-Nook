bebe149cb8e40e7f2735aa9aeb14fde683710f15
Thanate Dokmai <nook.microstock@gmail.com>
2024-10-16 15:27:19 +0700

    feather/add main content

---
+++ src/componant/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <>
+      <hr></hr>
+      <nav className='flex justify-between my-1'>
+        <div>
+          <span className='pl-10'>hh.</span>
+        </div>
+        <div>
+          <button className="border border-black rounded-2xl w-24 p-1 text-sm text-center mr-1">
+            Login
+          </button>
+          <button className="rounded-2xl w-24 p-1 text-white bg-black text-sm text-center">
+            Sign up
+          </button>
+        </div>
+      </nav>
+      <hr></hr>
+    </>
+  );
+}
+
+export default Navbar
